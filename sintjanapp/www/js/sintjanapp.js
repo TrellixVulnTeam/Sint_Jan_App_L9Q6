6702dc17c7718d5d43aa553fb9a6c995dc21ae02
jktechs <jjlkoppe@hotmail.com>
2021-04-13 12:46:08 +0200

    Revert "Refacter everything"
    This reverts commit 8589f7c2c2ba1cf64a42ea62649ef70a34491f58.

--- sintjanapp/www/js/sintjanapp.js
+++ sintjanapp/www/js/sintjanapp.js
@@ -1,49 +1,64 @@
+var linkRE = /https:\/\/somtoday\.nl\/oidc\?code=([a-zA-Z0-9._\-]*)&state=[a-zA-Z0-9.-_]*#/gm;
 var win;
 var access_token = "";
-function openApp(packageName, activityName, url){//Try to open an app. If the app can't be found redirect to page.
-	var sApp = cordova["plugins"]["startApp"].set({//paramaters for the openApp plugin
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav w3-container w3-card") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav w3-container w3-card";
+  }
+}
+function openApp(packageName, activityName, url){//nl.topicus.somtoday.leerling, nl.topicus.somtoday.leerlinglib.activity.SplashActivity, https://somtoday.nl/
+	var sApp = cordova["plugins"]["startApp"].set({ /* params */
 		"action":"ACTION_MAIN",
 		"category":"CATEGORY_DEFAULT",
 		"type":"text/css",
 		"package":packageName,
+		/*"uri":"file://data/index.html",*/
 		"flags":["FLAG_ACTIVITY_CLEAR_TOP","FLAG_ACTIVITY_CLEAR_TASK"],
 		"component": [packageName,activityName],
 		"intentstart":"startActivity",
 	});
-	sApp.check(function(values) {//Check if app exists on phone.
-		sApp.start(function() {//If app is found, start it 
-		}, function(error) {//if there is an error 
+	sApp.check(function(values) {
+		sApp.start(function() {
+		}, function(error) {
 			alert(error);
 		}, function() {
+			console.log("done");
 		});
-	}, function(error) {//If app can't be found, start site.
+	}, function(error) {
 		openPage(url);
 	});
 }
-function openPage(url){//Open page in inAppBrowser plugin
-	win = cordova.InAppBrowser.open(url,"_blank","location=yes");//Open page
-	win.addEventListener('loaderror',function(params) {//Check if there is an error
-		win.close();//Close the window
-		var xhr = new XMLHttpRequest();//Create Http request
+function openPage(url){
+	win = cordova.InAppBrowser.open(url,"_blank","location=yes");
+	win.addEventListener('loaderror',function(params) {
+		win.close();
+        var linkCode = "code"+params.url.substring(params.url.indexOf('='),params.url.length);
+		var xhr = new XMLHttpRequest();
 		var url = "https://production.somtoday.nl/oauth2/token";
-		xhr.open("POST", url);//Open Post request
-		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");//set Content header (were the infe comes from)
+		xhr.open("POST", url);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4) {//If the request return succesfully
-			access_token = JSON.parse(xhr.responseText).access_token;//Get Somtoday acces token
-			showName();//get user info
+		if (xhr.readyState === 4) {
+			//alert(xhr.responseText);
+			//alert(JSON.parse(xhr.responseText).access_token);
+			access_token = JSON.parse(xhr.responseText).access_token;
+			showName();
 		}};
-        var linkCode = "code"+params.url.substring(params.url.indexOf('='),params.url.length);//Create urlencoded data
 		var data = "grant_type=authorization_code&"+
 				   "client_id=D50E0C06-32D1-4B41-A137-A9A850C892C2&"+
 				   "client_secret=vDdWdKwPNaPCyhCDhaCnNeydyLxSGNJX&"+
 				   "redirect_uri=somtodayleerling%3A%2F%2Foauth%2Fcallback&"+
 				   "code_verifier=t9b9-QCBB3hwdYa3UW2U2c9hhrhNzDdPww8Xp6wETWQ&"+linkCode;
 	
-		xhr.send(data);//Send the request
+		xhr.send(data);
     });
 	
 }
+function loadStopCallBack() {alert("start");}
+function executeScriptCallBack(params) {alert(params);}
 function som(x){
 	if(!x)
 		openApp("nl.topicus.somtoday.leerling", "nl.topicus.somtoday.leerlinglib.activity.SplashActivity", "https://somtoday.nl/");
@@ -62,24 +77,71 @@ function itslearning(x){
 	else
 		openPage("https://lvo.itslearning.com/");
 }
-function onLoad() {//Set event when cordova is ready.
+// example.js file
+// Wait for device API libraries to load
+//
+function onLoad() {
     document.addEventListener("deviceready", onDeviceReady, false);
 }
+
+// device APIs are available
+//
 function onDeviceReady() {
-	//on deviceready
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
 }
-function showName() {
-	var xhr = new XMLHttpRequest();//Creates request
+function onPause() {
+    // Handle the pause event
+}
+
+function onResume() {
+    // Handle the resume event
+}
+
+function onMenuKeyDown() {
+    // Handle the menubutton event
+}
+function onSomtodayRedirect(event, callback) {
+	alert(event.url);
+	
+	callback(event.url);
+	
+	if(event.url.startsWith("https://somtoday.nl/oidc?code=")) {
+		var btn = document.createElement("p");   // Create a <p> element
+		btn.innerHTML = event.url;                   // Insert text
+		document.body.appendChild(btn);               // Append <button> to <body>
+		var linkCode = event.url.replace("https://somtoday.nl/oidc?","").split("&")[0]+"..";
+		//win.close();
+		var xhr = new XMLHttpRequest();
+		var url = "https://production.somtoday.nl/oauth2/token";
+		xhr.open("POST", url);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			alert(event.url);
+			alert(data);
+			alert(xhr.responseText);
+		}};
+		var data = "grant_type=authorization_code&"+
+				   "client_id=D50E0C06-32D1-4B41-A137-A9A850C892C2&"+
+				   "client_secret=vDdWdKwPNaPCyhCDhaCnNeydyLxSGNJX&"+
+				   "redirect_uri=somtodayleerling%3A%2F%2Foauth%2Fcallback&"+
+				   "code_verifier=t9b9-QCBB3hwdYa3UW2U2c9hhrhNzDdPww8Xp6wETWQ&"+linkCode;
+	
+		//xhr.send(data);
+	}
+}
+function showName(){
+	var xhr = new XMLHttpRequest();
 	var url = "https://api.somtoday.nl/rest/v1/leerlingen";
-	xhr.open("GET", url);//Opens get request
-	xhr.setRequestHeader("Authorization", 'Bearer '+access_token);//Set Auth token.
-	xhr.setRequestHeader("Accept", 'application/json');//Sets accept header to JSON format.
+	xhr.open("GET", url);
+	xhr.setRequestHeader("Authorization", 'Bearer '+access_token);
+	xhr.setRequestHeader("Accept", 'application/json');
 	xhr.onreadystatechange = function () {
-	if (xhr.readyState === 4) {//If Get request if succesfull send alert with name.
+	if (xhr.readyState === 4) {
+		
 		alert(JSON.parse(xhr.responseText).items[0].roepnaam+" "+JSON.parse(xhr.responseText).items[0].achternaam);// JSON.items[0].roepnaam+" "+xhr.responseJSON.items[0].achternaam);
 	}};
-	xhr.send();	//Sends request
-	//var btn = document.createElement("p");   // Create a <p> element
-	//btn.innerHTML = event.url;               // Insert text
-	//document.body.appendChild(btn);          // Append <button> to <body>
+	xhr.send();	
 }
