78f84f5efc22ef470b0b5d3dc9fbee6694d4926a
jktechs <jjlkoppe@hotmail.com>
2021-04-15 09:16:49 +0200

    refactoring

--- sintjanapp/www/js/sintjanapp.js
+++ sintjanapp/www/js/sintjanapp.js
@@ -3,6 +3,21 @@ var em = "";
 var pas = "";
 var access_token = "";
 var lvobuuid = "d091c475-43f3-494f-8b1a-84946a5c2142";
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+function onDeviceReady() {
+	const urlParams = new URLSearchParams(window.location.search);
+	loadLogin(function () {
+		if(urlParams.get('email') != null)
+			em = urlParams.get('email');
+		if(urlParams.get('password') != null)
+			pas = urlParams.get('password');
+		if(urlParams.get('rem') == "true")
+			saveLogin(em,pas);
+		login(showName);
+	});
+}
 function openApp(packageName, activityName, url){
 	var sApp = cordova["plugins"]["startApp"].set({ /* params */
 		"action":"ACTION_MAIN",
@@ -36,8 +51,6 @@ function openPage(url){
 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4) {
-			//alert(xhr.responseText);
-			//alert(JSON.parse(xhr.responseText).access_token);
 			access_token = JSON.parse(xhr.responseText).access_token;
 			showName();
 		}};
@@ -46,61 +59,15 @@ function openPage(url){
 				   //"client_secret=vDdWdKwPNaPCyhCDhaCnNeydyLxSGNJX&"+
 				   "redirect_uri=somtodayleerling%3A%2F%2Foauth%2Fcallback&"+
 				   "code_verifier=t9b9-QCBB3hwdYa3UW2U2c9hhrhNzDdPww8Xp6wETWQ&"+linkCode;
-	
 		xhr.send(data);
     });
-	
-}
-function som(x){
-	if(!x)
-		openApp("nl.topicus.somtoday.leerling", "nl.topicus.somtoday.leerlinglib.activity.SplashActivity", "https://somtoday.nl/");
-	else
-		openPage("https://somtoday.nl/");
-}
-function zermelo(x){
-	if(!x)
-		openApp("nl.zermelo.online.App","nl.zermelo.online.App.MainActivity","https://account.activedirectory.windowsazure.com/applications/signin/40a96122-51bb-430a-b504-6a225c51e676?tenantId=788de26b-bf5a-46d5-bb58-f35ff7bdd172");
-	else
-		openPage("https://account.activedirectory.windowsazure.com/applications/signin/40a96122-51bb-430a-b504-6a225c51e676?tenantId=788de26b-bf5a-46d5-bb58-f35ff7bdd172");
-}
-function itslearning(x){
-	if(!x)
-		openApp("com.itslearning.itslearningintapp","itslearning.app.common.ActivityAppStartingPoint","https://lvo.itslearning.com/");
-	else
-		openPage("https://lvo.itslearning.com/");
-}
-function onLoad() {
-    document.addEventListener("deviceready", onDeviceReady, false);
-}
-function onDeviceReady() {
-	//alert("test");
-	const urlParams = new URLSearchParams(window.location.search);
-	loadLogin(function () {if(urlParams.get('email'))
-	em = urlParams.get('email');
-if(urlParams.get('password'))
-	pas = urlParams.get('password');
-if(urlParams.get('rem') == "on")
-	saveLogin(em,pas);
-	login(showName);
-//var emel = document.createElement("p");
-//var pasel = document.createElement("p");
-//emel.innerText = em;
-//pasel.innerText = pas;
-//document.body.append(em);
-//document.body.append(document.createElement("br"));
-//document.body.append(pas);
-});
-	
 }
 function login(onLoginDone){
 	var url = "https://production.somtoday.nl/oauth2/token";
-
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url);
-	
 	xhr.setRequestHeader("Accept", "application/json");
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4) {
 		    //console.log(xhr.responseText);
@@ -108,86 +75,44 @@ function login(onLoginDone){
 			onLoginDone();
 	   }
 	};
-	
 	var data = "grant_type=password&username=d091c475-43f3-494f-8b1a-84946a5c2142\\"+em+"&password="+pas+"&scope=openid&client_id=D50E0C06-32D1-4B41-A137-A9A850C892C2"//9509466
 	xhr.send(data);
-	
 }
 function saveLogin(email, password){
 	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
-
-		//alert('file system open: ' + fs.name);
 		fs.root.getFile("login.bgi", { create: true, exclusive: false }, function (fileEntry) {
-	
-			//alert("fileEntry is file?" + fileEntry.isFile.toString());
 			writeFile(fileEntry, new Blob([email+','+password], { type: 'text/plain' }));
-	
 		}, onErrorCreateFile);
-	
 	}, onErrorLoadFs);
 	em = email;
 	pas = password;
 }
 function loadLogin(afterload){
 	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
-
-		//alert('file system open: ' + fs.name);
 		fs.root.getFile("login.bgi", { create: true, exclusive: false }, function (fileEntry) {
-	
-			//alert("fileEntry is file?" + fileEntry.isFile.toString());
 			readFile(fileEntry, function() {
-				    //alert("Successful file read: " + this.result);
-				    //displayFileData(fileEntry.fullPath + ": " + this.result);
 					em = this.result.split(",")[0];
 					pas = this.result.split(",")[1];
-				    //alert(em);
-				    //alert(pas);
 					afterload();
 			});
-	
 		}, onErrorCreateFile);
-	
 	}, onErrorLoadFs);
 }
 function writeFile(fileEntry, dataObj) {
-    // Create a FileWriter object for our FileEntry (log.txt).
     fileEntry.createWriter(function (fileWriter) {
-
         fileWriter.onwriteend = function() {
-            //alert("Successful file write...");
-            //readFile(fileEntry);
+            //onwriteend
         };
-
-        fileWriter.onerror = function (e) {
-            alert("Failed file write: " + e.toString());
-        };
-
+        fileWriter.onerror = onErrorReadFile;
         fileWriter.write(dataObj);
     });
 }
 function readFile(fileEntry, onReady) {
-
     fileEntry.file(function (file) {
         var reader = new FileReader();
-
         reader.onloadend = onReady;
-		//function() {
-        //    alert("Successful file read: " + this.result);
-        //    //displayFileData(fileEntry.fullPath + ": " + this.result);
-        //};
-
         reader.readAsText(file);
-
     }, onErrorReadFile);
-}
-function onErrorCreateFile(){
-	alert("create error");
-}
-function onErrorLoadFs(){
-	alert("load sys error");
-}
-function onErrorReadFile(){
-	alert("read error");
 }
 function onSomtodayRedirect(event, callback) {
 	alert(event.url);
@@ -232,3 +157,33 @@ function showName(){
 	}};
 	xhr.send();	
 }
+function som(x){
+	if(!x)
+		openApp("nl.topicus.somtoday.leerling", "nl.topicus.somtoday.leerlinglib.activity.SplashActivity", "https://somtoday.nl/");
+	else
+		openPage("https://somtoday.nl/");
+}
+function zermelo(x){
+	if(!x)
+		openApp("nl.zermelo.online.App","nl.zermelo.online.App.MainActivity","https://account.activedirectory.windowsazure.com/applications/signin/40a96122-51bb-430a-b504-6a225c51e676?tenantId=788de26b-bf5a-46d5-bb58-f35ff7bdd172");
+	else
+		openPage("https://account.activedirectory.windowsazure.com/applications/signin/40a96122-51bb-430a-b504-6a225c51e676?tenantId=788de26b-bf5a-46d5-bb58-f35ff7bdd172");
+}
+function itslearning(x){
+	if(!x)
+		openApp("com.itslearning.itslearningintapp","itslearning.app.common.ActivityAppStartingPoint","https://lvo.itslearning.com/");
+	else
+		openPage("https://lvo.itslearning.com/");
+}
+function onErrorCreateFile(){
+	alert("create error");
+}
+function onErrorLoadFs(){
+	alert("load sys error");
+}
+function onErrorReadFile(){
+	alert("read error");
+}
+function onErrorReadFile(e) {
+	alert("Failed file write: " + e.toString());
+};
